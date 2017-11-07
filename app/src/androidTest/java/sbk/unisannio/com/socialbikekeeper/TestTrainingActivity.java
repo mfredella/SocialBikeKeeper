682b4a0f3cf0ff1c6751a6f727c9ffc6a12abca6
carusosara <sara.caruso.1987@gmail.com>
2017-11-07 12:43:47 +0100

    Aggiunta del test per il passaggio a una nuova Activity dopo la selezione di Stop Aggiunta del test per la visualizzazione del Toast di errore per la selezione prematura di Stop

--- app/src/androidTest/java/sbk/unisannio/com/socialbikekeeper/TestTrainingActivity.java
+++ app/src/androidTest/java/sbk/unisannio/com/socialbikekeeper/TestTrainingActivity.java
@@ -1,15 +1,18 @@
 package sbk.unisannio.com.socialbikekeeper;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.junit.Test;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import java.util.Collection;
+
+import static android.support.test.runner.lifecycle.Stage.RESUMED;
 
 /**
  * Created by carusosara on 07/11/2017.
@@ -18,6 +21,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class TestTrainingActivity extends ActivityInstrumentationTestCase2<TrainingActivity> {
     private TrainingActivity tActivity;
     private TextView KM_value, cal_value, timer_value;
+    private Button start_button, stop_button;
 
     public TestTrainingActivity() {
         super(TrainingActivity.class);
@@ -38,6 +42,8 @@ public class TestTrainingActivity extends ActivityInstrumentationTestCase2<Train
     /*
     * Controllo se le TextView sono inizializzate correttamente
      * se sono invisibili alla creazione dell'activity
+     * se sono visibili alla selezione dello Start
+     * se la selezione di Stop prima di Start mostra un Toast di errore
     */
     @Test
     public void testTextView() throws InterruptedException {
@@ -51,5 +57,48 @@ public class TestTrainingActivity extends ActivityInstrumentationTestCase2<Train
 
         assertEquals(KM_value.getVisibility(), View.INVISIBLE);
         assertEquals(cal_value.getVisibility(), View.INVISIBLE);
+
+        start_button= (Button)tActivity.findViewById(R.id.start_button);
+        tActivity.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                start_button.performClick();
+            }
+        });
+        Thread.sleep(1000);
+
+        assertEquals(KM_value.getVisibility(), View.VISIBLE);
+        assertEquals(cal_value.getVisibility(), View.VISIBLE);
+
+        stop_button= (Button)tActivity.findViewById(R.id.stop_button);
+        tActivity.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                stop_button.performClick();
+            }
+        });
+        Thread.sleep(2000);
+        Activity activity = getActivityInstance();
+        boolean b = (activity instanceof  RisultatiActivity);
+        assertTrue(b);
+
+    }
+
+    public Activity getActivityInstance() {
+        final Activity[] activity = new Activity[1];
+        getInstrumentation().runOnMainSync(new Runnable( ) {
+            public void run() {
+                Activity currentActivity = null;
+                Collection resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+                if (resumedActivities.iterator().hasNext()){
+                    currentActivity = (Activity) resumedActivities.iterator().next();
+                    activity[0] = currentActivity;
+                }
+            }
+        });
+
+        return activity[0];
     }
 }
